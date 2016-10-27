15bc9b13e109086ed14904fd6f2b4d1a9c8a4ef5
rluisr <detteiu.x14th@gmail.com>
2016-10-27 07:55:00 +0000

    A

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -exu
-
+yum install ruby ruby-devel -y
+yum groupinstall Development Tools -y
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp -rf .vim ../
 cp -rf .vimrc ../

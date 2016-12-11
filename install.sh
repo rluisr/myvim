6c926ee9f3a4795e1d66650bcbaeb6b69da03055
luis <luis@Macintosh.local>
2016-12-12 07:35:40 +0900

    Support macOS

--- install.sh
+++ install.sh
@@ -1,6 +1,25 @@
 #!/bin/bash -exu
-yum install ruby ruby-devel rake -y
-yum groupinstall Development Tools -y
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cp -rf .vim ../
-cp -rf .vimrc ../
+
+function common() {
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+  cp -rf .vim ../
+  cp -rf .vimrc ../
+}
+
+function rhel_prepare() {
+  yum groupinstall Development Tools -y
+  yum install ruby ruby-devel rake -y
+}
+
+if [ "$(uname)" == 'Darwin' ]; then
+  common
+
+# Only rhel
+elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+  rhel_prepare
+  common
+
+else
+  echo "Your platform ($(uname -a)) is not supported."
+  exit 1
+fi
